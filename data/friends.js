625dea9a7cccd3ba12e56ebeaf94229c2db8b114
fodnb <Daniel.Mcghee6813@gmail.com>
2017-07-14 07:47:53 -0400

    added div close

--- data/friends.js
+++ data/friends.js
@@ -1,66 +1,56 @@
-var friend =  [{
-   "name":"Emperor Palpatine",
-   "photo": "cssFiles/images/palpatine.jpg",
-   "scores": [1,1,1,1,1,5,5,5,5,5,5,5],
-   "sound": "cssFiles/audio/emperor.mp3" 
- },
- {
-   "name":"Darth Maul",
-   "photo": "cssFiles/images/darthmaul.jpg",
-   "scores": [1,2,1,2,1,5,5,5,5,5,4,4],
-   "sound": "cssFiles/audio/emperor.mp3" 
- },
- {
-   "name":"Count Dooku",
-   "photo": "cssFiles/images/dooku.jpg",
-   "scores": [2,2,2,2,2,4,4,4,4,4,3,3],
-   "sound": "cssFiles/audio/emperor.mp3" 
- },
- {
-   "name":"The Inquisitor",
-   "photo": "cssFiles/images/inquisitor.jpg",
-   "scores": [2,3,2,3,2,3,3,3,3,3,2,2],
-   "sound": "cssFiles/audio/emperor.mp3"
- },
- {
-   "name":"Darth Vader",
-   "photo": "cssFiles/images/vader.jpg",
-   "scores": [3,3,3,3,3,3,3,3,3,3,1,2],
-   "sound": "cssFiles/audio/emperor.mp3"
- },
- {
-   "name":"Mace Windu",
-   "photo": "cssFiles/images/windu.jpg",
-   "scores": [3,3,3,3,3,3,3,3,3,3,2,1],
-   "sound": "cssFiles/audio/obiwan.mp3" 
- },
- {
-   "name":"Kit Fisto",
-   "photo": "cssFiles/images/kit.jpg",
-   "scores": [3,3,3,3,3,2,3,2,3,2,3,3],
-   "sound": "cssFiles/audio/obiwan.mp3" 
- },
- {
-   "name":"Yoda",
-   "photo": "cssFiles/images/yoda.jpg",
-   "scores":[4,4,4,4,4,2,2,2,2,2,2,2],
-   "sound": "cssFiles/audio/yoda.mp3" 
- },
- {
-   "name":"Obi Wan Kenobi",
-   "photo": "cssFiles/images/obiwan.jpg",
-   "scores":[5,5,5,5,5,1,2,1,2,1,1,2],
-   "sound": "cssFiles/audio/obiwan.mp3"  
- },
- {
-   "name":"Luke SkyWalker",
-   "photo": "cssFiles/images/luke.jpg",
-   "scores":[5,5,5,5,5,1,1,1,1,1,1,1],
-   "sound": "cssFiles/audio/luke.mp3"   
- }
+var friend = [{
+        "name": "Emperor Palpatine",
+        "photo": "cssFiles/images/palpatine.jpg",
+        "scores": [1, 1, 1, 1, 1, 5, 5, 5, 5, 5, 5, 5],
+        "sound": "cssFiles/audio/emperor.mp3"
+    }, {
+        "name": "Darth Maul",
+        "photo": "cssFiles/images/darthmaul.jpg",
+        "scores": [1, 2, 1, 2, 1, 5, 5, 5, 5, 5, 4, 4],
+        "sound": "cssFiles/audio/emperor.mp3"
+    }, {
+        "name": "Count Dooku",
+        "photo": "cssFiles/images/dooku.jpg",
+        "scores": [2, 2, 2, 2, 2, 4, 4, 4, 4, 4, 3, 3],
+        "sound": "cssFiles/audio/emperor.mp3"
+    }, {
+        "name": "The Inquisitor",
+        "photo": "cssFiles/images/inquisitor.jpg",
+        "scores": [2, 3, 2, 3, 2, 3, 3, 3, 3, 3, 2, 2],
+        "sound": "cssFiles/audio/emperor.mp3"
+    }, {
+        "name": "Darth Vader",
+        "photo": "cssFiles/images/vader.jpg",
+        "scores": [3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 1, 2],
+        "sound": "cssFiles/audio/emperor.mp3"
+    }, {
+        "name": "Mace Windu",
+        "photo": "cssFiles/images/windu.jpg",
+        "scores": [3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 2, 1],
+        "sound": "cssFiles/audio/obiwan.mp3"
+    }, {
+        "name": "Kit Fisto",
+        "photo": "cssFiles/images/kit.jpg",
+        "scores": [3, 3, 3, 3, 3, 2, 3, 2, 3, 2, 3, 3],
+        "sound": "cssFiles/audio/obiwan.mp3"
+    }, {
+        "name": "Yoda",
+        "photo": "cssFiles/images/yoda.jpg",
+        "scores": [4, 4, 4, 4, 4, 2, 2, 2, 2, 2, 2, 2],
+        "sound": "cssFiles/audio/yoda.mp3"
+    }, {
+        "name": "Obi Wan Kenobi",
+        "photo": "cssFiles/images/obiwan.jpg",
+        "scores": [5, 5, 5, 5, 5, 1, 2, 1, 2, 1, 1, 2],
+        "sound": "cssFiles/audio/obiwan.mp3"
+    }, {
+        "name": "Luke SkyWalker",
+        "photo": "cssFiles/images/luke.jpg",
+        "scores": [5, 5, 5, 5, 5, 1, 1, 1, 1, 1, 1, 1],
+        "sound": "cssFiles/audio/luke.mp3"
+    }
 
- ]
+]
 
 
 module.exports = friend;
-
